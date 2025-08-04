75328450304d96bcd89db7669b16127311626482
marion-borne <marion.borne@laplateforme.io>
2025-08-04 11:30:59 +0200

    Ajout de l'application web minimale (index.html) et d'un script de vérification Bash (check_app.sh)

---
+++ app/check_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "🛠️ Démarrage de la vérification de l'application..."
+
+if [ -f "./index.html" ]; then
+  echo "✅ Fichier index.html trouvé. Vérification réussie."
+  exit 0
+else
+  echo "❌ Fichier index.html manquant. Échec de la vérification."
+  exit 1
+fi
